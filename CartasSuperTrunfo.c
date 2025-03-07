dab40ecfaeb62ad2d6da38de7d22edde3387784b
web-will <marquessilva89@hotmail.com>
2025-03-07 00:40:59 +0000

    recriei estrutura switch, tudo ok. Falta apenas corrigir erro de nome da cidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -137,28 +137,43 @@ int main() {
             }
     } while (opcao1 == opcao2);
     
-    //Execução das escolhas dos atributos
-    switch (opcao1) {
-    case 1:
-        printf ("Carta 1= %s Carta 2= %s", cidade1, cidade2);
-        printf ("Atributos escolhidos: %d e %d\n", opcao1, opcao2);
-        printf ("Valor Carta 1: %lu Valor Carta 2: %lu\n", populacao1, populacao2);
-        if (populacao1 == populacao2) {
-            printf("Empatou");
-        }
-        if (populacao1 > populacao2)
-        {
-            printf("Carta 1 %s venceu!\n", cidade1);
-        } else {
-            printf("Carta 2 %s venceu!\n", cidade2);
-        }
+    //comparação dos atributos
+    float valor1_carta1, valor1_carta2, valor2_carta1, valor2_carta2;
 
-        
-    break;
-    default:
-        printf("Opção Inválida\n");
-    break;
+    switch (opcao1) {
+    case 1: valor1_carta1 = populacao1; valor1_carta2 = populacao2; break;
+    case 2: valor1_carta1 = area1; valor1_carta2 = area2; break;
+    case 3: valor1_carta1 = pib1; valor1_carta2 = pib2; break;
+    case 4: valor1_carta1 = pontos_turisticos1; valor1_carta2 = pontos_turisticos2; break;
+    case 5: valor1_carta1 = densidade_populacional1; valor1_carta2 = densidade_populacional2; break;
     }
-    
+   
+    switch (opcao2) {
+    case 1: valor2_carta1 = populacao1; valor2_carta2 = populacao2; break;
+    case 2: valor2_carta1 = area1; valor2_carta2 = area2; break;     
+    case 3: valor2_carta1 = pib1; valor2_carta2 = pib2; break;
+    case 4: valor2_carta1 = pontos_turisticos1; valor2_carta2 = pontos_turisticos2; break;
+    case 5: valor2_carta1 = densidade_populacional1; valor2_carta2 = densidade_populacional2; break;
+    }
+
+    //soma dos atributos
+    float soma_carta1 = valor1_carta1 + valor2_carta1;
+    float soma_carta2 = valor1_carta2 + valor2_carta2;
+
+    // --- Exibição do resultado ---
+    printf("\n--- Resultado da Comparação ---\n");
+    printf("%s vs %s\n", cidade1, cidade2);
+    printf("Atributos escolhidos: %d e %d\n", opcao1, opcao2);
+    printf("Valores: %0.2f vs %0.2f e %0.2f vs %0.2f\n", valor1_carta1, valor1_carta2, valor2_carta1, valor2_carta2);
+    printf("Soma total: %0.2f vs %0.2f\n", soma_carta1, soma_carta2);
+
+    if (soma_carta1 > soma_carta2) {
+        printf("%s venceu!\n", cidade1);
+    } else if (soma_carta1 < soma_carta2) {
+        printf("%s venceu!\n", cidade2);
+    } else {
+        printf("Empate!\n");
+    }
+
     return 0;
 }
